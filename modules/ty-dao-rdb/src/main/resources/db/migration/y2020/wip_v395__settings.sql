46344f87a1deab318531d4d4acece36a1ad25baa
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2020-08-04 12:13:34 +0200

    snooze cols.

--- modules/ty-dao-rdb/src/main/resources/db/migration/y2020/wip_v395__settings.sql
+++ modules/ty-dao-rdb/src/main/resources/db/migration/y2020/wip_v395__settings.sql
@@ -4,3 +4,6 @@ alter table settings3 add constraint settings_c_startofbodyhtml_len check (
     length(start_of_body_html) between 1 and 50000);
 
 
+alter table user_stats3 add column snooze_notfs_until timestamp;
+alter table user_stats3 add column after_snooze_then int;
+
